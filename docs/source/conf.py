3fb7896391c55461e67fa18d8a300db326287ab5
Brennon Bortz <brennon@brennonbortz.com>
2015-12-18 13:50:42 -0500

    Automatically link to numpy and scipy docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -366,4 +366,8 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python' : ('https://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+    'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None)
+}
